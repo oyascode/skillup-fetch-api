23278d6206b4920a95eda3268d1ac6ad93d8c3f3
oyascode <johnmikeloy@gmail.com>
2021-04-01 09:47:15 +0100

    added moreimages

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
+const moreImges = document.querySelector(".next");
 
-const auth = "563492ad6f917000010000019d6e521ad8f345798ad01d3e472ea042"
+const apiKey = "563492ad6f917000010000019d6e521ad8f345798ad01d3e472ea042"
 
-const next = document.querySelector(".next");
-const input = document.querySelector("input");
-const searchBtn = document.querySelector("search-btn");
-
- let pageNumber = 1;
- let query = "";
-//  let search = false;
-
-
-input.addEventListener("input", (e) => {
-  e.preventDefault();
-  query = e.target.value;  
-})
-
-const selectImages = async () => {
-  const imageData = await fetch(
+let pageNumber = 1;
+const getCuratedPhotos = async (pageNumber) => {
+  const response = await fetch(
     `https://api.pexels.com/v1/curated?per_page=20&${pageNumber}`,
     {
       method: "GET", 
       headers: {
         accept: "application/JSON",
-        authorization: auth
+        authorization: apiKey
       }
     }
   );
-  const response = await imageData.json();
-  response.photos.forEach(photo => {
+  const photoData = await response.json();
+  photoData.photos.forEach(photo => {
     const imageCard = document.createElement("div");
     imageCard.className = "card-img";
     imageCard.innerHTML = 
@@ -40,4 +28,9 @@ const selectImages = async () => {
   });
 } 
 
-selectImages(pageNumber);
+moreImges.addEventListener("click", () => {
+  pageNumber = pageNumber++;
+  getCuratedPhotos(pageNumber);
+})
+
+getCuratedPhotos(pageNumber);
